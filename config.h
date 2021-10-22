d12e4ae6551f3572d21a04f64ee7d3ddfcd31ca5
Nico Stranquist <nstranquist@gmail.com>
2021-10-22 03:34:42 -0500

    add interrupt and signal handling code. define some constants

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
 #define MAX_TIME 100
 #define MAX_PROCESSES 50
 #define DEFAULT_TIME 2
+#define SLEEP_TIME 100
--- oss.c
+++ oss.c
@@ -5,18 +5,35 @@
 #include <limits.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/ipc.h>
 #include <sys/types.h>
+// #include <sys/shm.h> // shouldn't need to use shared memory
+#include <signal.h>
+#include <sys/time.h>
 #include <sys/sem.h>
 #include <sys/stat.h>
-#include <sys/msg.h>
+#include <sys/msg.h> // use the message queues
 #include <getopt.h>
 
 #include "config.h"
 
-// Setup Interrupt Handler
+static void myhandler(int signum) {
+  if(signum == SIGINT) {
+    // is ctrl-c interrupt
+    perror("\nrunsim: Ctrl-C Interrupt Detected. Shutting down gracefully...\n");
+  }
+  else if(signum == SIGALRM) {
+    // is timer interrupt
+    perror("\nrunsim: Info: The time for this program has expired. Shutting down gracefully...\n");
+  }
+  else {
+    perror("\nrunsim: Warning: Only Ctrl-C and Timer signal interrupts are being handled.\n");
+    return; // ignore the interrupt, do not exit
+  }
 
-// Setup timer handler
+  // do more cleanup
 
+}
 
 int main(int argc, char *argv[]) {
   printf("Hello world!\n");
@@ -26,7 +43,8 @@ int main(int argc, char *argv[]) {
   // -s t for max seconds before termination
   // -l f specify a particular name for the log file
   int option;
-  int seconds;
+  int seconds = -1;
+  int sleepTime;
   char *logfileName = NULL;
   int val;
   while((option = getopt(argc, argv, "hsl:")) != -1) {
@@ -57,6 +75,52 @@ int main(int argc, char *argv[]) {
     printf("Non-option argument: %s\n", argv[i]);
   }
 
+  if(seconds != -1) {
+    printf("seconds exists: %d\n", seconds);
+  }
+  else {
+    printf("seconds is undefined. setting to default\n");
+    sleepTime = SLEEP_TIME;
+  }
+
+
+  // Setup intterupt handler
+  signal(SIGINT, myhandler);
+
+  // Start program timer
+  alarm(sleepTime);
+
 
   return 0;
+}
+
+
+// message queue
+
+
+
+
+
+// handlers and interrupts
+static int setupitimer(int sleepTime) {
+  struct itimerval value;
+  value.it_interval.tv_sec = 0;
+  value.it_interval.tv_usec = 0;
+  value.it_value.tv_sec = sleepTime; // alarm
+  value.it_value.tv_usec = 0;
+  return (setitimer(ITIMER_PROF, &value, NULL));
+}
+
+static int setupinterrupt(void) {
+  struct sigaction act;
+  act.sa_handler = myhandler;
+  act.sa_flags = 0;
+  return (sigemptyset(&act.sa_mask) || sigaction(SIGPROF, &act, NULL) || sigaction(SIGALRM, &act, NULL));
+}
+
+static int timerHandler(int s) {
+  int errsave;
+  errsave = errno;
+  write(STDERR_FILENO, "The time limit was reached\n", 1);
+  errno = errsave;
 }
